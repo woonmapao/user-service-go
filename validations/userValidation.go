14b50b00dd801020eb0009f612b5a00c40e1a6a6
Patarapong Limvipaveeanan <pat_opor@hotmail.com>
2023-12-24 11:36:15 +0700

    Completed username and email duplicate validate functions

--- validations/userValidation.go
+++ validations/userValidation.go
@@ -1,9 +1,20 @@
 package validations
 
+import (
+	"github.com/woonmapao/user-service-go/initializer"
+	"github.com/woonmapao/user-service-go/models"
+)
+
 func IsUsernameDuplicate(username string) bool {
-	return false
+	var count int64
+	initializer.DB.Model(&models.User{}).Where(
+		"username = ?", username).Count(&count)
+	return count > 0
 }
 
 func IsEmailDuplicate(email string) bool {
-	return false
+	var count int64
+	initializer.DB.Model(&models.User{}).Where(
+		"email = ?", email).Count(&count)
+	return count > 0
 }
