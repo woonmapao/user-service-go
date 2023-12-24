d395763e346c41d2649c422a9cf64b1d1631dceb
Patarapong Limvipaveeanan <pat_opor@hotmail.com>
2023-12-24 12:06:46 +0700

    Added initializer when starting a server

--- main.go
+++ main.go
@@ -2,7 +2,13 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/woonmapao/user-service-go/initializer"
 )
+
+func init() {
+	initializer.LoadEnvVariables()
+	initializer.DBInitializer()
+}
 
 func main() {
 
