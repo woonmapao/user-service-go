9b9de21b6dfa2a7bf5c147f8a4769542df79031a
Patarapong Limvipaveeanan <pat_opor@hotmail.com>
2024-01-03 18:07:38 +0700

    Reorder functions in controller

--- controllers/userController.go
+++ controllers/userController.go
@@ -7,14 +7,14 @@ import (
 
 	"github.com/gin-gonic/gin"
 	i "github.com/woonmapao/user-service-go/initializer"
-	"github.com/woonmapao/user-service-go/models"
+	m "github.com/woonmapao/user-service-go/models"
 	r "github.com/woonmapao/user-service-go/responses"
 	"gorm.io/gorm"
 )
 
-func AddUser(user *models.UserRequest, tx *gorm.DB) error {
+func AddUser(user *m.UserRequest, tx *gorm.DB) error {
 
-	adding := models.User{
+	adding := m.User{
 		Username: user.Username,
 		Email:    user.Email,
 		Password: user.Password,
@@ -27,9 +27,35 @@ func AddUser(user *models.UserRequest, tx *gorm.DB) error {
 	return nil
 }
 
-func UpdateUser(user *models.UserRequest, tx *gorm.DB) error {
+func GetUser(id int, db *gorm.DB) (*m.User, error) {
 
-	updating := models.User{
+	var user m.User
+	err := db.First(&user, id).Error
+	if err == gorm.ErrRecordNotFound {
+		return &user, errors.New("user not found")
+	}
+	if err != nil {
+		return &user, errors.New("something went wrong")
+	}
+	return &user, nil
+}
+
+func GetUsers(db *gorm.DB) (*[]m.User, error) {
+
+	var users []m.User
+	err := db.Find(&users).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return &users, errors.New("failed to fetch user")
+	}
+	if err == gorm.ErrRecordNotFound {
+		return &users, errors.New("no user found")
+	}
+	return &users, nil
+}
+
+func UpdateUser(user *m.UserRequest, tx *gorm.DB) error {
+
+	updating := m.User{
 		Username: user.Username,
 		Email:    user.Email,
 		Password: user.Password,
@@ -44,7 +70,7 @@ func UpdateUser(user *models.UserRequest, tx *gorm.DB) error {
 
 func DeleteUser(id int, tx *gorm.DB) error {
 
-	err := tx.Delete(&models.User{}, id).Error
+	err := tx.Delete(&m.User{}, id).Error
 	if err != nil {
 		tx.Rollback()
 		return errors.New("failed to delete user")
@@ -85,7 +111,7 @@ func CommitTrx(c *gin.Context, tx *gorm.DB) error {
 	return nil
 }
 
-func BindAndValidate(c *gin.Context, body *models.UserRequest) error {
+func BindAndValidate(c *gin.Context, body *m.UserRequest) error {
 
 	err := c.ShouldBindJSON(&body)
 	if err != nil {
@@ -101,30 +127,4 @@ func BindAndValidate(c *gin.Context, body *models.UserRequest) error {
 		)
 	}
 	return nil
-}
-
-func GetUser(id int, db *gorm.DB) (*models.User, error) {
-
-	var user models.User
-	err := db.First(&user, id).Error
-	if err == gorm.ErrRecordNotFound {
-		return &user, errors.New("user not found")
-	}
-	if err != nil {
-		return &user, errors.New("something went wrong")
-	}
-	return &user, nil
-}
-
-func GetUsers(db *gorm.DB) (*[]models.User, error) {
-
-	var users []models.User
-	err := db.Find(&users).Error
-	if err != nil && err != gorm.ErrRecordNotFound {
-		return &users, errors.New("failed to fetch user")
-	}
-	if err == gorm.ErrRecordNotFound {
-		return &users, errors.New("no user found")
-	}
-	return &users, nil
 }
